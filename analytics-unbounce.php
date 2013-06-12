e78c9782203ff6358ec57e084c4f4967b81aebc1
Ishan Sharma <i@ishan.co>
2013-06-12 14:15:48 +0530

    Bug fixes for saving and code injection

--- analytics-unbounce.php
+++ analytics-unbounce.php
@@ -4,13 +4,22 @@ Plugin Name: Analytics Unbounce
 Pluign URI: http://wpblogexperts.com/plugins
 Description: Adds Google Analytics tracking code to WordPress and fixes bounce rate measurement
 Version: 1.0
-Author: Ishan
+Author: Ishan Sharma
 Author URI: http://ishan.co
 License: GPL2
 */
 
-$unbounce_analytics_id = get_option('unbounce_analytics_id');
-$unbounce_analytics_input_id = '';
+/* Check if there is a stored Analytics ID in databsae.*/
+if (!get_option('unbounce_analytics_id'))
+{
+	add_option('unbounce_analytics_id'); // If key is not there, a blank value is created.
+}
+$unbounce_analytics_id = get_option('unbounce_analytics_id'); // Retrieve the value from database
+$unbounce_analytics_input_id = ''; // No input for now, so keeping it blank. 
+
+
+/* *** Add Code to Website *** */
+add_action('wp_footer', 'unbounce_tracking_code'); 
 
 function unbounce_tracking_code()
 {
@@ -32,6 +41,7 @@ function unbounce_tracking_code()
 	</script>
 _TRACKING_CODE_;
 
+	// Check if current user is logged in and is author or higher. Echo code only if condition is false. 
 	if (!current_user_can('edit_published_posts'))
 	{
 		echo $tracking_script;
@@ -41,13 +51,9 @@ _TRACKING_CODE_;
 		echo "<!--- Analytics Unbounce Plugin is working but not tracking you as you are admin-->";
 	}
 }
+/* Plugin front end work ends here, next is about options and database handling */
 
-add_action('wp_footer', 'unbounce_tracking_code');
-/* Plugin front end work ends here */
-?>
-
-<?php
-/* Next section adds the admin options. */
+/* *** Add Plugins Optinos Under Settings *** */
 add_action ('admin_menu', 'unbounce_admin_menu');
 
 function unbounce_admin_menu()
@@ -66,7 +72,6 @@ function unbounce_options()
 	if (array_key_exists('unbounce_submit', $_POST))
 	{
 		$unbounce_message = unbounce_check_analytics_key();
-		store_unbounce_analytics_key();
 	}
 	echo <<<_UNBOUNCE_OPTIONS_
 	<div id="wrap">
@@ -74,10 +79,10 @@ function unbounce_options()
 		$unbounce_message
 		<hr />
 		<form action="options-general.php?page=analytics-unbounce" method="post">
-			<label for="analytics_id">Enter Your Google Analytics ID</label> <br />
-			<input type="text" name="analytics_id" value="$unbounce_analytics_id"/> <br />
-			<input type="submit" value="Save" /> <hr />
-			<input type="hidden" name="unbounce_submit" />
+			<label for="analytics_id">Enter Your Google Analytics ID:</label> <br />
+			<input type="text" maxlength="14" name="analytics_id" value="$unbounce_analytics_id" class="regular-text" /> <br />
+			<input type="submit" value="Save" class="button button-primary" /> <hr />
+			<input type="hidden" name="unbounce_submit"/>
 		</form>
 	</div>
 _UNBOUNCE_OPTIONS_;
@@ -97,30 +102,23 @@ function unbounce_check_analytics_key()
 		else
 		{
 			$unbounce_analytics_input_id = $unbounce_analytics_local_id;
+			store_unbounce_analytics_key();
 			return '<div class="updated settings-error">Analytics ID saved</div>';
 		}
 	}
 }
-?>
 
-<?php
-/* This is some scary databse stuff!*/
-function unbounce_activate()
-{
-	add_option('unbounce_analytics_id');
-}
+
+/* This section has some scary databse stuff!*/
 
 function store_unbounce_analytics_key()
 	{
 		global $unbounce_analytics_input_id;
-		if ($unbounce_analytics_input_id != '')
+		global $unbounce_analytics_id;
+		if ($unbounce_analytics_input_id != $unbounce_anlytics_id)
 		{
+			$unbounce_analytics_id = $unbounce_analytics_input_id;
 			update_option('unbounce_analytics_id', $unbounce_analytics_input_id);
 		}
 	}
-?>
-
-<?php
-/* Some work to be done on activation*/
-	register_activation_hook(__FILE__ , 'unbounce_activate');
 ?>
